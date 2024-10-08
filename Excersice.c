6ae21cee5019d690e9a3bec600f24087b71d3fb1
DhruvP2006 <dhruv.pankhania@somaiya.edu>
2024-10-08 14:04:14 +0530

    A

---
+++ Excersice.c
@@ -0,0 +1,24 @@
+// To calculate area of a square
+// #include <stdio.h>
+
+// int main(){
+//   float side;
+//   printf("Enter the side of the Square:");
+//   scanf("%f", &side);
+//   printf("area of the square is: %f", side * side);
+//   return 0;
+// }
+
+// Area of cirlcle
+// #include<stdio.h>
+
+// int main(){
+//   float radius;
+//   printf("Enter the radius of the circle:");
+//   scanf("%f", &radius);
+//   printf("The area of the Circle is: %f", 3.14*radius*radius);
+//   return 0;
+// }
+
+// Type conversion
+#include<st
