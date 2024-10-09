33c2fffa8c4d1966fa23a9e7243996d4487113fb
DhruvP2006 <dhruv.pankhania@somaiya.edu>
2024-10-10 00:29:11 +0530

    loops

--- Hello.c
+++ Hello.c
@@ -1,36 +1,15 @@
 #include <stdio.h>
-#include<math.h>
 
 int main() {
-  int day;
-  printf("Enter day() : ");
-  scanf("%s", &day);
-  switch(day){
-    case 'm':
-    printf("Monday\n");
-    break;
-    case 't':
-    printf("Tuesday\n");
-    break;
-    case 'w':
-    printf("Wednesday\n");
-    break;
-    case 'T':
-    printf("Thursday\n");
-    break;
-    case 'f':
-    printf("Friday\n");
-    break;
-    case 's':
-    printf("Saturday\n");
-    break;
-    case 'S':
-    printf("Tuesday\n");
-    break;
-    default:
-    printf("Not a valid Day\n");
-    break;
-  }
-  return 0;
+    int i;
+  do{
+    printf("Enter a Number: ");
+    scanf("%d", &i);
+    printf("%d \n", i);
+    if(i%2 == 1){
+      break;
+    }
+  }while(1);
+return 0;
 }
 
