81dd051a8b66f38b5f29cb981f48c92fce8aab97
DhruvP2006 <dhruv.pankhania@somaiya.edu>
2025-02-21 09:59:53 +0530

    Assignment 5

--- PIC Experiments/EXP5.1_C4-1_216.c
+++ PIC Experiments/EXP5.1_C4-1_216.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main(){
-  int vowel = 0, consonants = 0;
+  int vowel = 0, consonants = 0, specialChar = 0;
   char str[100];
   printf("Enter a string: ");
   gets(str);
@@ -11,13 +11,19 @@ int main(){
       vowel++;
     }
     else{
-      if((int)str[i]<65 && (int)str[i]>90 && (int)str[i]>96 && (int)str[i]<123){}
+      if(((int)str[i]>65 && (int)str[i]<90) || ((int)str[i]>96 && (int)str[i]<123)){
+        consonants++;
+      }
       else{
-      consonants++;
+        if(str[i]==' '){}
+        else{
+          specialChar++;
+        }
     }}
 }
 
 printf("The Number of Vowels in the String is: %d", vowel);
-printf("\nThe Number of Vowels in the String is: %d", consonants);
+printf("\nThe Number of Consonants in the String is: %d", consonants);
+printf("\nThe Number of Special Characters in the String is: %d", specialChar);
   return 0;
 }
--- PIC Experiments/EXP5.2_C4-1_216.c
+++ PIC Experiments/EXP5.2_C4-1_216.c
@@ -2,6 +2,15 @@
 #include<string.h>
 
 int main(){
-
+  int name[100], grade[100], nameCpy[100];
+  printf("Enter the Name of the Student: ");
+  gets(name);
+  printf("Enter the Grade of the Student: ");
+  gets(grade);
+  printf("The Length of the Student`s Name is: %s\nThe Length of the Student`s Grade is: %s", strlen(name), strlen(grade));
+  strcpy(nameCpy, name);
+  printf("Student s Name copied into another String: %s", nameCpy);
+  strcat(name, " - - Excellent Student");
+  printf("%s", name);
   return 0;
 }
