5ffe95b2e803367408048efc51b1f1af5d87f460
DhruvP2006 <dhruv.pankhania@somaiya.edu>
2025-04-22 00:15:08 +0530

    expt 9

--- Excersice.c
+++ Excersice.c
@@ -1002,3 +1002,58 @@ int peek()
 
   return 0;
 }
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  // use of malloc
+  // int *ptr;
+  // int n;
+  // printf("Enter the size of the array: ");
+  // scanf("%d", &n);
+  // ptr = (int *)malloc(n * sizeof(int));
+  // for (int i = 0; i < n; i++)
+  // {
+  //   printf("Enter a Number\n");
+  //   scanf("%d", &ptr[i]);
+  // }
+  // for (int i = 0; i < n; i++)
+  // {
+  //   printf("Entered Number is: %d\n", ptr[i]);
+  // }
+
+  // Use of calloc
+  // int *ptr;
+  // int n;
+  // printf("Enter the size of the array: ");
+  // scanf("%d", &n);
+  // ptr = (int *)calloc(n, sizeof(int));
+  // for (int i = 0; i < n; i++)
+  // {
+  //   printf("Enter a Number\n");
+  //   scanf("%d", &ptr[i]);
+  // }
+  // for (int i = 0; i < n; i++)
+  // {
+  //   printf("Entered Number is: %d\n", ptr[i]);
+  // }
+
+  // Use of realloc
+  int *ptr;
+  int n;
+  printf("Enter the size of the array: ");
+  scanf("%d", &n);
+  ptr = (int *)calloc(n, sizeof(int));
+  for (int i = 0; i < n; i++)
+  {
+    printf("Enter a Number\n");
+    scanf("%d", &ptr[i]);
+  }
+  for (int i = 0; i < n; i++)
+  {
+    printf("Entered Number is: %d\n", ptr[i]);
+  }
+  return 0;
+}
